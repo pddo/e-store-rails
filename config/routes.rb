b78e5392ac18bbfea37496fd78c44faaf2e86af7
pddo <dduyphuc@gmail.com>
2019-06-16 22:54:03 +0700

    Add scope '/admin' to products routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static#home'
   get '/home', to: 'static#home'
   get '/index', to: redirect('/')
   get '/contact', to: 'static#contact'
+
+  scope '/admin' do
+    root to: "products#index"
+    resources :products
+  end
 end
